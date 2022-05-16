ef79dc13549a8a23f9633db7f2e18cbac4b502bb
Yo Yamamoto <48309954+yoyamamoto@users.noreply.github.com>
2022-05-16 14:48:29 +0900

    顔文字を切り替える関数とuseStateの組み合わせ練習をした

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onClickCountUp = () => {
     setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
   };
   const contentStyle = {
     color: "#000",
     fontSize: "26px"
   };
-  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "#000" }}>Webのプルタブ</h1>
@@ -18,7 +22,10 @@ const App = () => {
       </p>
       <ColorfulMessage color="bule">2022年5月12日 Yo Yamamoto</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップボタン</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>( ﾟДﾟ)</p>}
     </>
   );
 };
